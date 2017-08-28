9680bb8e152acf86d0cca34500e314c05e200321
Tim Dager <dageta01@ipfw.edu>
2017-08-28 11:29:39 -0400

    'phonework' is working, needs comments and cleanup'

--- ece485_hw01_tim/hw01_phonework/hw01_phonework.h
+++ ece485_hw01_tim/hw01_phonework/hw01_phonework.h
@@ -3,6 +3,11 @@
 
 #define TRUE 1
 #define FALSE 0
+
+#define TO_INT(x) (x)-'0'
+
+#include <assert.h>
+
 /***************************************************************/
 const uint8_t phoneWorkLUT[8][3] = 					{	
 																							{'A','B','C'},	//2
@@ -20,12 +25,16 @@ const uint8_t phoneWorkLUT[8][3] = 					{
 uint8_t validate_number(uint8_t *ptrNumber);
 void print_failure(uint8_t *ptrNumber);
 void print_number_words(uint8_t *ptrNumber, uint8_t numberOfWords);
+void print_as_word(uint8_t *ptrNumber);
 void print_number(uint8_t *ptrNumber);
-uint8_t phone_number_to_int(uint8_t *ptrNumber)
+void print_as_letter(uint8_t *ptrNumber, uint8_t numShift, uint8_t numLUT);
+
+/*uint8_t phone_number_to_int(uint8_t *ptrNumber)
 {
 	return *(ptrNumber) - '0';
 }
-
+*/
+																						
 uint8_t validate_number(uint8_t *ptrNumber)
 {
 	uint8_t i = 0;
@@ -36,7 +45,7 @@ uint8_t validate_number(uint8_t *ptrNumber)
 			return FALSE;
 		}
 	}
-	//if all fails and goes wrong, the number is returned as a failure
+	//if the above fails then no numbers are 0;
 	return TRUE;
 }
 /************************************************************/
@@ -54,13 +63,59 @@ void print_number(uint8_t *ptrNumber)
 	{
 		printf("%c", *(ptrNumber + i));
 	}
+	printf("\n");
 }
 
+/************************************************************
+void print_as_words(uint8_t *ptrNumber, uint8_t lookUpNumber)
+{
+	uint8_t i = 0;
+	//assert(0 <= lookUpNumber && lookUpNumber <= 2);
+
+	for(i = 0; i < 7; i++)
+	{
+		print_as_letter(ptrNumber, i, lookUpNumber);
+	}
+	printf("\n");
+}
+************************************************************/
+void print_as_letter(uint8_t *ptrNumber, uint8_t numShift, uint8_t numLUT)
+{
+	printf("%c", phoneWorkLUT[TO_INT(*(ptrNumber + numShift)) - 2][numLUT]);
+}
 /************************************************************/
 void print_number_words(uint8_t *ptrNumber, uint8_t numberOfWords)
 {
 	uint8_t i = 0;
 	uint8_t j = 0;
+	uint8_t k = 0;
+	uint8_t stepNumber[7] = {0,0,0,0,0,0,0}; //used to represent which letter that number is on;
+	
+	uint8_t count = 0;
+	printf("The entered number is ");
+	print_number(ptrNumber);
+	
+	for(i = 0; i < numberOfWords; i++)
+	{
+		for(j = 0; j < 7; j++)
+		{
+			if(stepNumber[j] > 2)
+			{
+				stepNumber[j] = 0;
+				stepNumber[j - 1] += 1;
+			}
+			else if(stepNumber[0] > 2)
+			{
+				break;
+			}
+			print_as_letter(ptrNumber, j, (int) stepNumber[j]);
 
+
+		}
+		stepNumber[6] += 1;
+		count++;
+		printf("\n");
+	}
+	printf("count");
 }
 #endif
